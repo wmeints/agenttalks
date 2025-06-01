8f44dbc836d24438efb7c8b22dcdd61eba1c3aec
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-06-01 10:46:18 +0200

    Add error mapping

--- reader-api/src/main/java/nl/fizzylogic/newscast/reader/clients/content/ContentClient.java
+++ reader-api/src/main/java/nl/fizzylogic/newscast/reader/clients/content/ContentClient.java
@@ -1,10 +1,14 @@
 package nl.fizzylogic.newscast.reader.clients.content;
 
+import org.eclipse.microprofile.graphql.Mutation;
+
+import io.smallrye.graphql.client.typesafe.api.ErrorOr;
 import io.smallrye.graphql.client.typesafe.api.GraphQLClientApi;
 import nl.fizzylogic.newscast.reader.clients.content.model.ContentSubmission;
 import nl.fizzylogic.newscast.reader.clients.content.model.SummarizeContent;
 
 @GraphQLClientApi(configKey = "content-api")
 public interface ContentClient {
-    ContentSubmission summarizeContent(SummarizeContent input);
+    @Mutation
+    ErrorOr<ContentSubmission> summarizeContent(SummarizeContent input);
 }
