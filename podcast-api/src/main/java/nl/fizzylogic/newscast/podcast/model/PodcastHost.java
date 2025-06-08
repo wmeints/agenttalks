0917210531de8e62583710dc28b919fd82f2ddef
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-06-08 08:12:22 +0200

    Remove unused builder logic in the podcast host

--- podcast-api/src/main/java/nl/fizzylogic/newscast/podcast/model/PodcastHost.java
+++ podcast-api/src/main/java/nl/fizzylogic/newscast/podcast/model/PodcastHost.java
@@ -37,11 +37,7 @@ public class PodcastHost extends PanacheEntityBase {
         this.index = index;
     }
 
-    public int index; // The index to sort hosts by
-
-    public static PodcastHostBuilder builder() {
-        return new PodcastHostBuilder();
-    }
+    public int index;
 
     public static PodcastHost findByIndex(int index) {
         return find("index", index).firstResult();
@@ -49,42 +45,5 @@ public class PodcastHost extends PanacheEntityBase {
 
     public static PodcastHost findByName(String name) {
         return find("name", name).firstResult();
-    }
-
-    public static class PodcastHostBuilder {
-        private final PodcastHost host;
-
-        public PodcastHostBuilder() {
-            this.host = new PodcastHost();
-        }
-
-        public PodcastHostBuilder withName(String name) {
-            host.name = name;
-            return this;
-        }
-
-        public PodcastHostBuilder withStyleInstructions(String styleInstructions) {
-            host.styleInstructions = styleInstructions;
-            return this;
-        }
-
-        public PodcastHostBuilder withLanguagePatterns(String languagePatterns) {
-            host.languagePatterns = languagePatterns;
-            return this;
-        }
-
-        public PodcastHostBuilder withIndex(int index) {
-            host.index = index;
-            return this;
-        }
-
-        public PodcastHostBuilder withVoiceId(String voiceId) {
-            host.voiceId = voiceId;
-            return this;
-        }
-
-        public PodcastHost build() {
-            return host;
-        }
     }
 }
