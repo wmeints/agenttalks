f8e7309b58569597b94eca91f5ab02c974579eec
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-06-18 20:31:17 +0200

    Fix import statements

--- content-api/src/main/java/nl/fizzylogic/newscast/content/model/ContentSubmission.java
+++ content-api/src/main/java/nl/fizzylogic/newscast/content/model/ContentSubmission.java
@@ -1,6 +1,11 @@
 package nl.fizzylogic.newscast.content.model;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.eclipse.microprofile.graphql.Type;
+
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import io.quarkus.panache.common.Parameters;
 import io.quarkus.panache.common.Sort;
@@ -11,12 +16,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
-import org.eclipse.microprofile.graphql.Type;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Type
 @Entity(name = "content_submission")
