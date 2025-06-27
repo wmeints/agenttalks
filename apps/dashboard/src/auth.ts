9cb914a2081d8a30103721253679e46ee2bbe53f
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-06-27 19:23:59 +0200

    Add authentication to the frontend

---
+++ apps/dashboard/src/auth.ts
@@ -0,0 +1,11 @@
+import { SvelteKitAuth } from '@auth/sveltekit';
+import Keycloak from '@auth/sveltekit/providers/keycloak';
+import { KEYCLOAK_ISSUER, KEYCLOAK_CLIENT_ID, KEYCLOAK_CLIENT_SECRET } from '$env/static/private';
+
+export const { handle, signIn, signOut } = SvelteKitAuth({
+    providers: [Keycloak({
+        issuer: KEYCLOAK_ISSUER,
+        clientId: KEYCLOAK_CLIENT_ID,
+        clientSecret: KEYCLOAK_CLIENT_SECRET,
+    })],
+});
