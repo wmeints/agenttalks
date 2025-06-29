e9ed39cef6664cc62c8920db64ade120557bb6b2
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-06-29 06:12:24 +0200

    Add support for retrieving application statistics

--- apps/content-api/src/main/java/nl/infosupport/agenttalks/content/graph/ContentGraph.java
+++ apps/content-api/src/main/java/nl/infosupport/agenttalks/content/graph/ContentGraph.java
@@ -1,31 +1,31 @@
 package nl.infosupport.agenttalks.content.graph;
 
-import java.time.LocalDate;
-import java.util.List;
-
-import org.eclipse.microprofile.graphql.GraphQLApi;
-import org.eclipse.microprofile.graphql.Mutation;
-import org.eclipse.microprofile.graphql.Query;
-
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import nl.infosupport.agenttalks.content.eventbus.ContentSubmissionCreated;
 import nl.infosupport.agenttalks.content.eventbus.EventPublisher;
-import nl.infosupport.agenttalks.content.graph.input.CreatePodcastEpisode;
-import nl.infosupport.agenttalks.content.graph.input.MarkAsProcessed;
-import nl.infosupport.agenttalks.content.graph.input.MarkForProcessing;
-import nl.infosupport.agenttalks.content.graph.input.SubmitContent;
-import nl.infosupport.agenttalks.content.graph.input.SummarizeContent;
+import nl.infosupport.agenttalks.content.graph.input.*;
 import nl.infosupport.agenttalks.content.model.ContentSubmission;
 import nl.infosupport.agenttalks.content.model.PodcastEpisode;
+import nl.infosupport.agenttalks.content.model.ApplicationStatistics;
+import nl.infosupport.agenttalks.content.service.ApplicationStatisticsCalculator;
 import nl.infosupport.agenttalks.content.service.ContentSubmissions;
+import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Mutation;
+import org.eclipse.microprofile.graphql.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @GraphQLApi
 @RequestScoped
 public class ContentGraph {
     @Inject
     ContentSubmissions contentSubmissions;
+
+    @Inject
+    ApplicationStatisticsCalculator applicationStatisticsCalculator;
 
     @Inject
     EventPublisher eventPublisher;
@@ -38,6 +38,11 @@ public class ContentGraph {
     @Query
     public List<ContentSubmission> findProcessableSubmissions(LocalDate startDate, LocalDate endDate) {
         return contentSubmissions.findProcessableSubmissions(startDate, endDate);
+    }
+
+    @Query
+    public ApplicationStatistics statistics() {
+        return applicationStatisticsCalculator.getApplicationStatistics();
     }
 
     @Mutation
