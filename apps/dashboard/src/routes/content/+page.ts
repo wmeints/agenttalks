f37f831fd4454a65fd12be9f8ccd1501cb3a342c
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-06-29 20:44:31 +0200

    Add logic to submit content

--- apps/dashboard/src/routes/content/+page.ts
+++ apps/dashboard/src/routes/content/+page.ts
@@ -6,11 +6,3 @@ export const _ContentQueryVariables: ContentQueryVariables = () => (
         pageSize: 10,
     }
 );
-
-// export const load: PageLoad = async (event) => {
-//     console.log("Loading content items...");
-
-//     return {
-//         ...await load_ContentQuery({ event, variables: { pageIndex: 0, pageSize: 10 } }),
-//     }
-// };
