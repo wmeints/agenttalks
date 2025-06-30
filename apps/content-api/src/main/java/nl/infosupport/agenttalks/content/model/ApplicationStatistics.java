9427f886a9942374f37b7156ce094e0c38a23b3b
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-06-30 07:39:07 +0200

    Slim down the code

--- apps/content-api/src/main/java/nl/infosupport/agenttalks/content/model/ApplicationStatistics.java
+++ apps/content-api/src/main/java/nl/infosupport/agenttalks/content/model/ApplicationStatistics.java
@@ -1,7 +1,12 @@
 package nl.infosupport.agenttalks.content.model;
 
+import io.micrometer.common.lang.NonNull;
+
 public class ApplicationStatistics {
+    @NonNull
     public long totalEpisodes;
+
+    @NonNull
     public long submissionsLastWeek;
 
     public ApplicationStatistics(long totalEpisodes, long submissionsLastWeek) {
