daafec585fc36d3308c7ae28625bd060e35537c1
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-06-30 07:56:00 +0200

    Fixed an issue with login logic

--- apps/dashboard/src/routes/+layout.server.ts
+++ apps/dashboard/src/routes/+layout.server.ts
@@ -5,7 +5,7 @@ export const load: LayoutServerLoad = async (event) => {
     const session = await event.locals.auth();
 
     if (!session) {
-        throw redirect(302, '/auth/signin/keycloak');
+        throw redirect(302, '/auth/signin/');
     }
 
     return {
