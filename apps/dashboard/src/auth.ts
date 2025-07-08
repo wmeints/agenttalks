b1560ebf72dfb27bc5357adf112deb31e16291ee
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-07-08 09:52:15 +0200

    Move configuration file to right location

--- apps/dashboard/src/auth.ts
+++ apps/dashboard/src/auth.ts
@@ -1,32 +1,34 @@
-import { SvelteKitAuth, type DefaultSession } from '@auth/sveltekit';
-import Keycloak from '@auth/sveltekit/providers/keycloak';
-import { getKeycloakConfig } from '$lib/server/config';
+import { SvelteKitAuth } from "@auth/sveltekit";
+import Keycloak from "@auth/sveltekit/providers/keycloak";
+import { getKeycloakConfig } from "$lib/config";
 
-declare module '@auth/core/types' {
-    interface Session extends DefaultSession {
-        accessToken?: string;
-    }
+declare module "@auth/core/types" {
+  interface Session extends DefaultSession {
+    accessToken?: string;
+  }
 }
 
 const keycloakConfig = getKeycloakConfig();
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
-    providers: [Keycloak({
-        issuer: keycloakConfig.issuer,
-        clientId: keycloakConfig.clientId,
-        clientSecret: keycloakConfig.clientSecret,
-    })],
-    callbacks: {
-        async jwt({ token, account }) {
-            if (account) {
-                token.accessToken = account.access_token;
-            }
+  providers: [
+    Keycloak({
+      issuer: keycloakConfig.issuer,
+      clientId: keycloakConfig.clientId,
+      clientSecret: keycloakConfig.clientSecret,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
 
-            return token;
-        },
-        async session({ session, token }) {
-            session.accessToken = token.accessToken as string;
-            return session;
-        }
-    }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken as string;
+      return session;
+    },
+  },
 });
