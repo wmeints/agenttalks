8c75fe1c421c5c83c1fbdb7542bd9a311503bb3e
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-07-23 07:59:24 +0200

    Fix annotations

--- src/main/java/nl/infosupport/agenttalks/content/SubmitContentRequest.java
+++ src/main/java/nl/infosupport/agenttalks/content/SubmitContentRequest.java
@@ -1,6 +1,9 @@
 package nl.infosupport.agenttalks.content;
 
+import org.eclipse.microprofile.graphql.NonNull;
+
 public class SubmitContentRequest {
+    @NonNull
     public String url;
     public String instructions;
 }
