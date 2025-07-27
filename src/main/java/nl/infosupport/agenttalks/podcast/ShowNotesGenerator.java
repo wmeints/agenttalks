aa54aa017891706a983f5f153cb2e06c004961ba
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-07-27 16:28:27 +0200

    Add failure handling

--- src/main/java/nl/infosupport/agenttalks/podcast/ShowNotesGenerator.java
+++ src/main/java/nl/infosupport/agenttalks/podcast/ShowNotesGenerator.java
@@ -1,11 +1,19 @@
 package nl.infosupport.agenttalks.podcast;
 
+import java.time.temporal.ChronoUnit;
+
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
+
+import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
 
 @RegisterAiService
-
 public interface ShowNotesGenerator {
+    @SystemMessage("You're the editor for AgentTalks.")
     @UserMessage(fromResource = "prompts/generate-show-notes.md")
+    @Timeout(value = 2, unit = ChronoUnit.MINUTES)
+    @Retry(maxRetries = 5, delay = 30, delayUnit = ChronoUnit.SECONDS)
     String generateShowNotes(String serializedPodcastScript, String serializedContentSubmissions);
 }
