ad06b04d3d9fdf7a69c8cf2565359df9ab2bef04
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-07-27 16:28:43 +0200

    Improve timeout behavior

--- src/main/java/nl/infosupport/agenttalks/podcast/PodcastScriptGenerator.java
+++ src/main/java/nl/infosupport/agenttalks/podcast/PodcastScriptGenerator.java
@@ -3,6 +3,7 @@ package nl.infosupport.agenttalks.podcast;
 import java.time.temporal.ChronoUnit;
 
 import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
@@ -10,9 +11,10 @@ import io.quarkiverse.langchain4j.RegisterAiService;
 
 @RegisterAiService
 public interface PodcastScriptGenerator {
-    @Retry(maxRetries = 5, delay = 30, delayUnit = ChronoUnit.SECONDS)
     @SystemMessage(fromResource = "prompts/podcast-script/system-instructions.txt")
     @UserMessage(fromResource = "prompts/podcast-script/user-instructions.txt")
+    @Timeout(value = 2, unit = ChronoUnit.MINUTES)
+    @Retry(maxRetries = 5, delay = 30, delayUnit = ChronoUnit.SECONDS)
     PodcastScript generatePodcastScript(
             String firstHostName,
             String secondHostName,
